1d0d9bb395ffc9fa68aed69e22cb982d7f38bfdc
David Dawkins <david.dawkins@gmail.com>
2023-02-16 23:13:12 +0000

    Latest fable, webpack, pin to .NET6

--- webpack.config.js
+++ webpack.config.js
@@ -6,19 +6,14 @@ var path = require("path");
 
 module.exports = {
     mode: "development",
-    entry: "./src/App.fsproj",
+    entry: "./src/App.fs.js",
     output: {
         path: path.join(__dirname, "./public"),
         filename: "bundle.js",
     },
     devServer: {
+        publicPath: "/",
         contentBase: "./public",
         port: 8080,
-    },
-    module: {
-        rules: [{
-            test: /\.fs(x|proj)?$/,
-            use: "fable-loader"
-        }]
     }
 }
